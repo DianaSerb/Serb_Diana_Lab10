00b91443f03de5a39d49dff274e489a23a3141e4
DIANA ȘERB <diana.serb@stud.ubbcluj.ro>
2020-12-09 12:56:52 +0200

    Add project files.

---
+++ Serb_Diana_Lab10.Android/Data/ShoppingListDatabase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Serb_Diana_Lab10.Droid.Data
+{
+    class ShoppingListDatabase
+    {
+    }
+}
